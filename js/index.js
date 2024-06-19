34763f0e747f320a966c3325cd91fddf86f343c6
Rikuto <orikutobasuke@gmail.com>
2024-06-19 12:25:07 +0900

    webゼミ

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
 // マウスストーカー用class取得
-const mouse = document.querySelector(".mousuStalker");
+const mouse = document.querySelector(".mouseStalker");
 
 // mousuStalkerの処理呼びたし
 document.addEventListener('mousemove', mousuStalker);
 
 // mousuStalkerの処理関数
-function mousuStalker() {
-
+function mousuStalker(e) {
+    mouse.style.top = e.clientY - 25 + 'px';
+    mouse.style.left = e.clientX - 25 + 'px';
 }
