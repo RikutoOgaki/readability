bc066774f27a087c2e8293f5e1dd18b2e9c574e7
Rikuto <orikutobasuke@gmail.com>
2024-06-19 10:05:18 +0900

    mousuStalkerの作成開始

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// マウスストーカー用class取得
+const mouse = document.querySelector(".mousuStalker");
+
+// mousuStalkerの処理呼びたし
+document.addEventListener('mousemove', mousuStalker);
+
+// mousuStalkerの処理関数
+function mousuStalker() {
+
+}
