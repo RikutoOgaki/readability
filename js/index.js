384932d611ac4e19e10a42f8827e11e4b0d1a5aa
Rikuto <orikutobasuke@gmail.com>
2024-06-19 14:57:02 +0900

    マウスストーカー実装完了

--- js/index.js
+++ js/index.js
@@ -1,11 +1,23 @@
+// マウスストーカー実装 ここから
+
 // マウスストーカー用class取得
 const mouse = document.querySelector(".mouseStalker");
 
 // mousuStalkerの処理呼びたし
-document.addEventListener('mousemove', mousuStalker);
+// document.addEventListener('mousemove', mousuStalker);
 
 // mousuStalkerの処理関数
-function mousuStalker(e) {
-    mouse.style.top = e.clientY - 25 + 'px';
-    mouse.style.left = e.clientX - 25 + 'px';
-}
+// function mousuStalker(e) {
+//     mouse.style.transform = 'translate(' + e.clientX + 'px,' + e.clientY + 'px)';
+// }
+
+// 疑問点
+// functionで関数を作って実装してしっかりと機能ごとに分けるか、コードの短さを重視して実装するか
+
+document.addEventListener('mousemove', (e) => {
+    mouse.style.transform = 'translate(' + e.clientX + 'px,' + e.clientY + 'px)';
+})
+
+// マウスストーカー実装 ここまで
+
+
