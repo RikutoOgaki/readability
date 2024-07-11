0708bd37ac7be7e3040aeb39f01479fc7ccbc206
Rikuto <orikutobasuke@gmail.com>
2024-07-11 14:18:29 +0900

    細かな修正

--- js/index.js
+++ js/index.js
@@ -3,17 +3,6 @@
 // マウスストーカー用class取得
 const mouse = document.querySelector(".mouseStalker");
 
-// mousuStalkerの処理呼びたし
-// document.addEventListener('mousemove', mousuStalker);
-
-// mousuStalkerの処理関数
-// function mousuStalker(e) {
-//     mouse.style.transform = 'translate(' + e.clientX + 'px,' + e.clientY + 'px)';
-// }
-
-// 疑問点
-// functionで関数を作って実装してしっかりと機能ごとに分けるか、コードの短さを重視して実装するか
-
 document.addEventListener('mousemove', (e) => {
     mouse.style.transform = 'translate(' + e.clientX + 'px,' + e.clientY + 'px)';
 })
